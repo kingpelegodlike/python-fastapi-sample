d89c7950cbdbca983620d59e943891a8f6637e5d
Xavier DUVAL <kingpelegodlike@gmail..com>
2025-11-23 19:28:26 +0100

    Donwloaad and Upload profiles with directory and NLSA parameter

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
+import os
 from typing import Union, Annotated
 
 from fastapi import FastAPI, File, UploadFile, HTTPException
-from fastapi.responses import FileResponse
+from fastapi.responses import FileResponse, HTMLResponse
 from pydantic import BaseModel
 
 app = FastAPI()
@@ -13,7 +14,84 @@ class Item(BaseModel):
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World"}
+    # return {"Hello": "World"}
+    content = """
+<body>
+<p><strong>Download Profile</strong></p>
+<form method="GET" id="downloadForm">
+    <input type="hidden" name="dummy" value="placeholder">
+    
+    <div>
+        <label for="dirName">Directory Name (profiles_type_1 or profiles_type_2):</label>
+        <input type="text" id="dirName" name="dirName" required>
+    </div>
+    <br>
+    <div>
+        <label for="nlsa">NLSA (number):</label>
+        <input type="text" id="nlsa" name="nlsa" required pattern="[0-9]+">
+        <small> (Must be a number)</small>
+    </div>
+    <br>
+    <button type="submit">Download Profile</button>
+</form>
+<hr>
+<p><strong>Upload Profile</strong></p>
+<form method="POST" id="uploadForm" enctype="multipart/form-data">
+    <input type="hidden" name="dummy" value="placeholder">
+    
+    <div>
+        <label for="dirName">Directory Name (profiles_type_1 or profiles_type_2):</label>
+        <input type="text" id="dirName" name="dirName" required>
+    </div>
+    <br>
+    <div>
+        <label for="nlsa">NLSA (number):</label>
+        <input type="text" id="nlsa" name="nlsa" required pattern="[0-9]+">
+        <small> (Must be a number)</small>
+    </div>
+    <br>
+    <div>
+        <label for="fileInput">Select File to Upload:</label>
+        <input type="file" id="fileInput" name="file" required>
+    </div>
+    <br>
+    <button type="submit">Upload Profile</button>
+</form>
+<script>
+    document.getElementById('downloadForm').addEventListener('submit', function(event) {
+        // Prevent the default form submission
+        event.preventDefault();
+
+        // Get the values from the input fields
+        const dirNameValue = document.getElementById('dirName').value;
+        const nlsaValue = document.getElementById('nlsa').value;
+
+        // Construct the dynamic action URL
+        const dynamicAction = `/dowloadFile/${dirNameValue}/${nlsaValue}`;
+
+        // Set the form action and submit
+        this.action = dynamicAction;
+        this.submit();
+    });
+    document.getElementById('uploadForm').addEventListener('submit', function(event) {
+        // Prevent the default form submission
+        event.preventDefault();
+
+        // Get the values from the input fields
+        const dirNameValue = document.getElementById('dirName').value;
+        const nlsaValue = document.getElementById('nlsa').value;
+
+        // Construct the dynamic action URL
+        const dynamicAction = `/uploadFile/${dirNameValue}/${nlsaValue}`;
+
+        // Set the form action and submit
+        this.action = dynamicAction;
+        this.submit();
+    });
+</script>
+</body>
+    """
+    return HTMLResponse(content=content)
 
 
 @app.get("/items/{item_id}")
@@ -24,23 +102,29 @@ async def read_item(item_id: int, q: Union[str, None] = None):
 async def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
 
-@app.get("/dowloadFile/{file_name}")
-async def download_file(file_name: str):
+@app.get("/dowloadFile/{dir_name}/{nlsa}")
+async def download_file(dir_name: str, nlsa: int):
+    file_name = f"nlsa{nlsa}.json"
+    file_path = os.path.join(dir_name, file_name)
     return FileResponse(
-        path=file_name,
+        path=file_path,
         media_type='application/octet-stream', # General-purpose binary data
         filename=file_name     # The name the user will see and save
     )
 
-@app.post("/uploadfile/")
-async def create_upload_file(file: UploadFile):
+@app.post("/uploadFile/{dir_name}/{nlsa}")
+async def create_upload_file(dir_name: str, nlsa: int, file: UploadFile):
+    profiles_dir_list = ["profiles_type_1", "profiles_type_2"]
+    if dir_name not in profiles_dir_list:
+        raise HTTPException(status_code=500, detail=f'{dir_name} not in {profiles_dir_list}')
+    file_path = os.path.join(dir_name, f"nlsa{nlsa}.json")
     try:
         contents = file.file.read()
-        with open(file.filename, 'wb') as f:
+        with open(file_path, 'wb') as f:
             f.write(contents)
     except Exception:
         raise HTTPException(status_code=500, detail='Something went wrong')
     finally:
         file.file.close()
 
-    return {"message": f"Successfully uploaded {file.filename}"}
+    return {"message": f"Successfully uploaded {file.filename} to {file_path}"}
